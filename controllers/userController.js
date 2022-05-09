ac17671c246060b3bb1c99802d51d8dfdcf492fd
Paras0-7 <parasrawat937@gmail.com>
2022-05-09 18:03:11 +0530

    update functionalities

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,6 @@
+const AppError = require('./../utils/appError');
+const catchAsync = require('./../utils/catchAsync');
+
 exports.getAllUsers = function (req, res) {
   res.status(500).json({
     status: 'error',
@@ -29,3 +32,17 @@ exports.deleteUser = function (req, res) {
     message: 'This route is not yet defined',
   });
 };
+
+exports.updateMe = catchAsync(async function (req, res, next) {
+  // Create error if user POSTs password data
+
+  if (req.body.password || req.body.passwordConfirm)
+    return next(
+      new AppError(
+        'This route is not for password updates. Please ise /updatePassword',
+        400
+      )
+    );
+});
+
+//
