4a593c07a38e30f7b5fca2677fd37adacb843608
Paras0-7 <parasrawat937@gmail.com>
2022-08-22 19:05:36 +0530

    mongoose

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const dotenv = require('dotenv');
-
+const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABSE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('Database Connected'));
 const app = require('./app');
 
 const port = process.env.PORT;
