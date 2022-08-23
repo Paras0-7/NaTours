c0a9b30ffed0cfd6d592f1f908e8d4a7e8195ecc
Paras0-7 <parasrawat937@gmail.com>
2022-08-23 20:36:30 +0530

    model and schema

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
-  process.env.DATABSE_PASSWORD
+  process.env.DATABASE_PASSWORD
 );
 
 mongoose
@@ -13,7 +13,33 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false,
   })
-  .then(() => console.log('Database Connected'));
+  .then((con) => console.log('Database Connected'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 45,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Forest Hiker',
+  rating: 4.7,
+  price: 497,
+});
+
+testTour.save().then((doc) => console.log(doc));
 const app = require('./app');
 
 const port = process.env.PORT;
