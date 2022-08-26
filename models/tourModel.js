8ba00c27fdd80399f84e9d90645dccb354d061df
Paras0-7 <parasrawat937@gmail.com>
2022-08-26 16:06:41 +0530

    custom validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -7,6 +7,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name must have less or equal than 40 characeter'],
+      minlength: [40, 'A tour name more have less or equal than 10 characeter'],
     },
     duration: {
       type: Number,
@@ -21,10 +23,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour  must have a difficulty'],
+      enum: {
+        values: ['easy', ' medium', 'difficult'],
+        message: 'Diffulty is either easy, meduim, or difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
@@ -34,7 +42,15 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price'],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          return val < this.price;
+        },
+        message: 'Discount price should be lower than the regular price',
+      },
+    },
     summary: {
       type: String,
       trim: true,
