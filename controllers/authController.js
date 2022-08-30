923022331ac7263700af3ac3f1674c4838244e65
Paras0-7 <parasrawat937@gmail.com>
2022-08-30 13:12:10 +0530

    signup function

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,11 @@
 const User = require('./../models/userModel');
-
-exports.signUp = async function (req, res) {
-  try {
-    const newUser = await User.create(req.body);
-    res.status(201).json({
-      status: 'Success',
-      data: {
-        User: newUser,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      Status: 'Fail',
-      message: err,
-    });
-  }
-};
+const catchAsync = require('./../utils/catchAsync');
+exports.signUp = catchAsync(async function (req, res, next) {
+  const newUser = await User.create(req.body);
+  res.status(201).json({
+    status: 'Success',
+    data: {
+      User: newUser,
+    },
+  });
+});
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -25,7 +26,25 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, ' Please confirm your password'],
+    validate: {
+      validator: function (value) {
+        return this.password === value;
+      },
+      message: 'The password do not match',
+    },
   },
+});
+
+// document middleware
+
+userSchema.pre('save', async function (next) {
+  // only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // hash the password
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
